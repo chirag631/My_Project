999afaa418d499b4ea796e29809832c3f18e892c
chirag631 <05042000cdk@gmail.com>
2021-01-06 18:49:56 +0530

    Updates

---
+++ precache-manifest.8f23c9622ed93324b3ea4d985392c732.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c96328f878c2fa11e4601e023af533c1",
+    "url": "/My_Project/index.html"
+  },
+  {
+    "revision": "0a73a1ca6c83252e093c",
+    "url": "/My_Project/static/css/2.5dc55297.chunk.css"
+  },
+  {
+    "revision": "15bad799c7a1b0dae330",
+    "url": "/My_Project/static/css/main.686842f3.chunk.css"
+  },
+  {
+    "revision": "0a73a1ca6c83252e093c",
+    "url": "/My_Project/static/js/2.0ff3658e.chunk.js"
+  },
+  {
+    "revision": "d2266803016540034565984c8bb59069",
+    "url": "/My_Project/static/js/2.0ff3658e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15bad799c7a1b0dae330",
+    "url": "/My_Project/static/js/main.15bdfbc0.chunk.js"
+  },
+  {
+    "revision": "2061ecb599ab4d809a04",
+    "url": "/My_Project/static/js/runtime-main.a58d9ea9.js"
+  }
+]);
